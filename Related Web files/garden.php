89ed48d765ce3efc65307af2f5a447306efe8d38
RobinChenRichmond <guanyu.chen@richmond.edu>
2016-05-24 14:06:36 -0400

    update web files
    add some css
    Have been able to test if the user has right username and pw, if right,
    jump to guanyuc.com

--- Related Web files/garden.php
+++ Related Web files/garden.php
@@ -5,33 +5,75 @@
 	<script language="javascript" type="text/javascript" src="jquery.js"></script>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" >
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style type="text/css">
+body {
+  /*background: linear-gradient(90deg, white, gray);*/
+  background-color: #C6D7F2;
+}
+
+body, h1, p {
+  font-family: "Helvetica Neue", "Segoe UI", Segoe, Helvetica, Arial, "Lucida Grande", sans-serif;
+  font-weight: normal;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+
+.container {
+  margin-left:  auto;
+  margin-right:  auto;
+  margin-top: 177px;
+  max-width: 1170px;
+  padding-right: 15px;
+  padding-left: 15px;
+}
+
+.row:before, .row:after {
+  display: table;
+  content: " ";
+}
+
+h1 {
+  font-size: 48px;
+  font-weight: 300;
+  margin: 0 0 20px 0;
+}
+
+.lead {
+  font-size: 21px;
+  font-weight: 200;
+  margin-bottom: 20px;
+}
+
+p {
+  color: red;
+  margin: 0 0 10px;
+}
+
+a {
+  color: #3282e6;
+  text-decoration: none;
+}
+</style>
 </head>
 
 <body>
 <a href = "http://guanyuc.com/home.html"> go back to the home page</a>
 <br>
 
+<p>  
+极品装备，一秒刷爆
+<br>
+皇城PK，胜者为王
+<br>
+屠龙宝刀，点击就送
+<br>
+单挑BOSS，怒刷装备
+<br>
+1.76经典版，来这里找回曾经的兄弟！
+<br>
+</p>
 
-<div id="output">this element will be accessed by jquery and this text replaced</div>
-<script id="source" language="javascript" type="text/javascript">
-  $(function () 
-  {
-    //-----------------------------------------------------------------------
-    // 2) Send a http request with AJAX http://api.jquery.com/jQuery.ajax/
-    //-----------------------------------------------------------------------
-    $.ajax({                                      
-      url: 'process.php',                  //the script to call to get data          
-      data: "",                        //you can insert url argumnets here to pass to api.php
-                                       //for example "id=5&parent=6"
-      dataType: 'json',                //data format      
-      success: function(data)          //on recieve of reply
-      {
-        var name = data[0];
-        $('#output').html("<b>name: </b>"+name);
-      } 
-    });
-  }); 
-</script>
 
 用户名： <input id="username" type="text">
 <br>
@@ -39,19 +81,33 @@
 
 
 
-<script type="text/javascript">
+<script id="source" language="javascript" type="text/javascript">
 
+	  
+	  
 	function myFunction()
 	{	
 		var x=document.getElementById("username").value;
 		var y=document.getElementById("passcode").value;
-		if(x=="0628")
-			{
-			self.location='home.html';
+		var result;
+		
+		$.ajax({                                      
+		      url: 'process.php',                  //the script to call to get data       
+		      data: {id:x,pw:y},                        //you can insert url argumnets here to pass to api.php
+		                                       //for example "id=5&parent=6"
+		      dataType: 'json',                //data format      
+		      success: function(data)          //on recieve of reply
+		      {
+		        result = data;
+			if(result == 1){
+				alert(result);
+				//self.location='home.html';
+			} else{
+				alert("用户名或者密码不对= =");
 			}
-		else{
-			alert("密码不对呀= =");
-		}
+		        
+		      } 
+		});
 	}
 </script>
 <p><button input id = "enter" type="button" onclick="myFunction()">点击进入</button></p>
--- Related Web files/process.php
+++ Related Web files/process.php
@@ -2,9 +2,26 @@
 $conn = mysql_connect('localhost','guanyuchen123','cgy123824');
 
 mysql_select_db('gameguanyu',$conn);
-$query = mysql_query('SELECT name FROM Profession');
-$r=mysql_fetch_row($query);
 
-echo json_encode($r);
+$user_id = $_GET['id'];
+$user_id = mysql_real_escape_string($user_id);
+$user_pw = $_GET['pw'];
+$user_pw = mysql_real_escape_string($user_pw);
+
+if($user_id == "" or $user_pw == ""){
+	echo json_encode(2);
+}
+
+$query = "SELECT pw FROM Game_User WHERE id = '$user_id'";
+$query = mysql_query($query);
+
+$r=mysql_fetch_row($query);
+$r=implode(" ",$r);
+
+if ($r == $user_pw) {
+    echo json_encode(1);
+} else {
+    echo json_encode(2);
+}
 
 ?>
